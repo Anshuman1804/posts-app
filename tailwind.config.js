b4d02c21332b68950f4320f1f5609e754dbfdcd6
Anshuman1804 <anshumanparihar97@gmail.com>
2024-08-04 23:56:08 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Include all JavaScript and TypeScript files in the src directory
+    './public/index.html',         // Include your index.html file if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
